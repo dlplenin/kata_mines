26cc9219b35ab4a40be8f60dbdc92903ee1ad383
Diego <dlplenin@yahoo.es>
2016-12-07 15:49:46 -0500

    fix client package versions

--- client/app/scripts/gameoflife/controller.js
+++ client/app/scripts/gameoflife/controller.js
@@ -3,90 +3,84 @@
 angular.module('GameOfLife')
 
 .controller('gameoflife', function($scope) {
-    $scope.controller_loaded = 'GameOfLife loaded!';
+  $scope.controller_loaded = 'GameOfLife loaded!';
+  $scope.board_size = 5;
+  
+  $scope.build_board = function(board_size) {
+    $scope.board_size = board_size;
+    $scope.original_game_board = _(0).range($scope.board_size)
+      .map(function() {
+        return _(0).range($scope.board_size)
+          .map(function() {
+            return Math.random() < 0.3 ? 1 : 0;
+          });
+      });
+    $scope.game_board = JSON.parse(JSON.stringify($scope.original_game_board));
+  };
+
+
+
+  $scope.build_board_pattern = function(option) {
     $scope.board_size = 5;
-
-    $scope.build_board = function(board_size) {
-       $scope.board_size = board_size;
-        $scope.original_game_board = _(0).range($scope.board_size)
-            .map(function() {
-                return _(0).range($scope.board_size)
-                    .map(function() {
-                        return Math.random() < 0.3 ? 1 : 0;
-                    });
-            });
-        $scope.game_board = JSON.parse(JSON.stringify($scope.original_game_board));
-    };
-
-    
-
-    $scope.build_board_pattern = function(option) {
-       $scope.board_size = 5;
-        $scope.original_game_board = [
-                [0, 0, 0, 0, 0],
-                [0, 0, 1, 0, 0],
-                [0, 0, 1, 0, 0],
-                [0, 0, 1, 0, 0],
-                [0, 0, 0, 0, 0]
-            ];
-
-        $scope.game_board = JSON.parse(JSON.stringify($scope.original_game_board));
-    };
+    $scope.original_game_board = [
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0]
+    ];
+    $scope.game_board = JSON.parse(JSON.stringify($scope.original_game_board));
+  };
 
 
-    $scope.run_next_generation = function() {
-        _($scope.original_game_board).each(function(element, index, row) {
-            var parent_index = index;
-            _(row).each(function(element, index, row) {
-                $scope.calculate_next_generation(parent_index, index);
-            });
-        });
-         $scope.original_game_board = JSON.parse(JSON.stringify($scope.game_board));
-    };
+  $scope.run_next_generation = function() {
+    _($scope.original_game_board).each(function(element, index, row) {
+      var parent_index = index;
+      _(row).each(function(element, index, row) {
+        $scope.calculate_next_generation(parent_index, index);
+      });
+    });
+    $scope.original_game_board = JSON.parse(JSON.stringify($scope.game_board));
+  };
 
 
-    $scope.calculate_next_generation = function(row_index, col_index) {
-        //$scope.game_board = _($scope.original_game_board).clone();
+  $scope.calculate_next_generation = function(row_index, col_index) {
+    //$scope.game_board = _($scope.original_game_board).clone();
 
-        //$scope.game_board = JSON.parse(JSON.stringify($scope.original_game_board));
+    //$scope.game_board = JSON.parse(JSON.stringify($scope.original_game_board));
 
-        //console.log(row_index, col_index);
+    //console.log(row_index, col_index);
 
 
-        var temp_value_current_cell = $scope.original_game_board[row_index][col_index];
-        $scope.original_game_board[row_index][col_index] = 0;
-        //console.log($scope.game_board);
-        var neighbors = [];
-        _($scope.original_game_board.slice(Math.max(row_index - 1, 0), row_index + 2))
-            .each(function(row) {
-                neighbors = neighbors.concat(row.slice(Math.max(col_index - 1, 0), col_index + 2));
-            });
-        $scope.original_game_board[row_index][col_index] = temp_value_current_cell;
+    var temp_value_current_cell = $scope.original_game_board[row_index][col_index];
+    $scope.original_game_board[row_index][col_index] = 0;
 
-        //console.log(neighbors);
-        var total_neighbors = 0;
-        total_neighbors = _(neighbors).reduce(function(memo, num) {
-            return memo + num;
-        }, 0);
+    var neighbors = [];
+    _($scope.original_game_board.slice(Math.max(row_index - 1, 0), row_index + 2))
+      .each(function(row) {
+        neighbors = neighbors.concat(row.slice(Math.max(col_index - 1, 0), col_index + 2));
+      });
+    $scope.original_game_board[row_index][col_index] = temp_value_current_cell;
 
-        
-        if (total_neighbors < 2 || total_neighbors > 3) {
-            $scope.game_board[row_index][col_index] = 0;
-            console.log(total_neighbors);
-        }
-        if (total_neighbors === 3) {
-            $scope.game_board[row_index][col_index] = 1;
-            console.log(total_neighbors);
-        }
-        //console.log($scope.game_board);
-       
-    };
+    var total_neighbors = 0;
+    total_neighbors = _(neighbors).reduce(function(memo, num) {
+      return memo + num;
+    }, 0);
+
+    if (total_neighbors < 2 || total_neighbors > 3) {
+      $scope.game_board[row_index][col_index] = 0;
+    }
+    if (total_neighbors === 3) {
+      $scope.game_board[row_index][col_index] = 1;
+    }
+
+  };
 })
 
 .config(function($routeProvider) {
-    $routeProvider
-        .when('/gameoflife', {
-            templateUrl: 'scripts/gameoflife/views/gameoflife.html',
-            controller: 'gameoflife'
-        });
+  $routeProvider
+    .when('/gameoflife', {
+      templateUrl: 'scripts/gameoflife/views/gameoflife.html',
+      controller: 'gameoflife'
+    });
 });
--- client/app/scripts/gameoflife/specs/controller_spec.js
+++ client/app/scripts/gameoflife/specs/controller_spec.js
@@ -1,166 +1,169 @@
 'use strict';
 
-describe('Controller: gameoflife', function () {
+describe('Controller: gameoflife', function() {
 
-    beforeEach(module('GameOfLife'));
-    beforeEach(module('underscore'));
+  beforeEach(module('GameOfLife'));
+  beforeEach(module('underscore'));
 
-    var scope;
+  var scope;
 
-    beforeEach(inject(function ($rootScope, $controller) {
-        scope = $rootScope.$new();
-        $controller('gameoflife', { $scope: scope });
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    $controller('gameoflife', {
+      $scope: scope
+    });
+  }));
+
+  describe('On instance', function() {
+    it('should set "controller_loaded" variable in scope', function() {
+      expect(scope.controller_loaded).toContain('loaded');
+    });
+  });
+
+  describe('when going to /gameoflife', function() {
+
+    var route, location, rootScope, httpBackend;
+
+    beforeEach(inject(function($route, $location, $rootScope, $httpBackend) {
+      route = $route;
+      location = $location;
+      rootScope = $rootScope;
+      httpBackend = $httpBackend;
+
+      httpBackend.when('GET', 'scripts/gameoflife/views/gameoflife.html').respond('<div></div>');
     }));
 
-    describe('On instance', function () {
-        it('should set "controller_loaded" variable in scope', function () {
-        expect(scope.controller_loaded).toContain('loaded');
-        });
+    afterEach(function() {
+      httpBackend.verifyNoOutstandingExpectation();
+      httpBackend.verifyNoOutstandingRequest();
     });
 
-    describe('when going to /gameoflife', function () {
+    it('should use gameoflife.html and controller', function() {
+      expect(route.current).toBeUndefined();
 
-        var route, location, rootScope, httpBackend;
+      location.path('/gameoflife');
 
-        beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
-        route = $route;
-        location = $location;
-        rootScope = $rootScope;
-        httpBackend = $httpBackend;
+      httpBackend.flush();
 
-        httpBackend.when('GET', 'scripts/gameoflife/views/gameoflife.html').respond('<div></div>');
-        }));
+      expect(route.current.templateUrl).toBe('scripts/gameoflife/views/gameoflife.html');
+      expect(route.current.controller).toBe('gameoflife');
+    });
 
-        afterEach(function () {
-        httpBackend.verifyNoOutstandingExpectation();
-        httpBackend.verifyNoOutstandingRequest();
-        });
+  });
 
-        it('should use gameoflife.html and controller', function () {
-        expect(route.current).toBeUndefined();
+  /*Diego*/
+  describe('On instance', function() {
+    it('must have a limit to the board', function() {
+      expect(typeof scope.board_size).toBe('number');
+    });
 
-        location.path('/gameoflife');
+    it('must exist an array with 1 and 0 numbers in random positions', function() {
+      scope.build_board(scope.board_size);
+      expect(scope.game_board[0][0]).toBeLessThan(2);
+      expect(scope.game_board[0][0]).toBeGreaterThan(-1);
 
-        httpBackend.flush();
-
-        expect(route.current.templateUrl).toBe('scripts/gameoflife/views/gameoflife.html');
-        expect(route.current.controller).toBe('gameoflife');
-        });
+      expect(scope.game_board[0][1]).toBeLessThan(2);
+      expect(scope.game_board[0][1]).toBeGreaterThan(-1);
 
     });
 
-    /*Diego*/
-    describe('On instance', function () {
-        it('must have a limit to the board', function () {
-            expect(typeof scope.board_size).toBe('number');
-        });
+  });
 
-        it('must exist an array with 1 and 0 numbers in random positions', function () {
-            expect(scope.game_board[0][0]).toBeLessThan(2);
-            expect(scope.game_board[0][0]).toBeGreaterThan(-1);
+  describe('When user clic Nex Generation button', function() {
+    
+    it('Should Any live cell with fewer than two live neighbours dies, as if caused by under-population.', function() {
+      scope.original_game_board = [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+      ];
+      scope.game_board = JSON.parse(JSON.stringify(scope.original_game_board));
+      scope.calculate_next_generation(0, 1);
+      expect(scope.game_board[0][1]).toBe(0);
 
-            expect(scope.game_board[0][1]).toBeLessThan(2);
-            expect(scope.game_board[0][1]).toBeGreaterThan(-1);
-            
-        });     
+
+      scope.original_game_board = [
+        [0, 1, 0, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 1, 1],
+      ];
+      scope.game_board = JSON.parse(JSON.stringify(scope.original_game_board));
+      scope.calculate_next_generation(0, 1);
+      expect(scope.game_board[0][1]).toBe(0);
+    });
+
+    it('Should Any live cell with two or three live neighbours lives on to the next generation.', function() {
+      scope.original_game_board = [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+      ];
+      scope.game_board = JSON.parse(JSON.stringify(scope.original_game_board));
+      scope.calculate_next_generation(1, 0);
+      expect(scope.game_board[1][0]).toBe(1);
+
+      scope.original_game_board = [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 0, 0]
+      ];
+      scope.game_board = JSON.parse(JSON.stringify(scope.original_game_board));
+      scope.calculate_next_generation(1, 0);
+      expect(scope.game_board[1][0]).toBe(1);
+
+      scope.original_game_board = [
+        [0, 1, 0],
+        [0, 1, 1],
+        [1, 1, 1]
+      ];
+      scope.game_board = JSON.parse(JSON.stringify(scope.original_game_board));
+      scope.calculate_next_generation(0, 1);
+      expect(scope.game_board[0][1]).toBe(1);
+    });
+
+    it('Should Any live cell with more than three live neighbours dies, as if by over-population.', function() {
+      scope.original_game_board = [
+        [0, 1, 0],
+        [1, 1, 0],
+        [1, 1, 0]
+      ];
+      scope.game_board = JSON.parse(JSON.stringify(scope.original_game_board));
+      scope.calculate_next_generation(1, 1);
+      expect(scope.game_board[1][1]).toBe(0);
+    });
+
+    it('should Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.', function() {
+      scope.original_game_board = [
+        [0, 0, 1],
+        [0, 1, 1],
+        [1, 1, 1]
+      ];
+      scope.game_board = JSON.parse(JSON.stringify(scope.original_game_board));
+      scope.calculate_next_generation(0, 1);
+      expect(scope.game_board[0][1]).toBe(1);
+
+      scope.original_game_board = [
+        [0, 0, 1],
+        [0, 1, 1],
+        [1, 1, 1]
+      ];
+      scope.game_board = JSON.parse(JSON.stringify(scope.original_game_board));
+      scope.calculate_next_generation(1, 0);
+      expect(scope.game_board[1][0]).toBe(1);
+
+      scope.original_game_board = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0]
+      ];
+scope.game_board = JSON.parse(JSON.stringify(scope.original_game_board));
+      scope.calculate_next_generation(2, 1);
+      expect(scope.game_board[2][1]).toBe(1);
 
     });
 
-    describe('When user clic Nex Generation button', function () {
-       
-/*       Any live cell with fewer than two live neighbours dies, as if caused by under-population. no importa estado anterior
-Any live cell with two or three live neighbours lives on to the next generation.
-Any live cell with more than three live neighbours dies, as if by over-population.
-Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.*/
-
-
-
-        it('Should Any live cell with fewer than two live neighbours dies, as if caused by under-population.', function () {
-            scope.original_game_board = [
-                [0, 1, 0],
-                [0, 1, 0],
-                [0, 1, 0]
-            ];
-            scope.calculate_next_generation(0,1);
-            expect(scope.game_board[0][1]).toBe(0);
-
-
-            scope.original_game_board = [
-
-                [0,   1,   0,   0],
-                [0,   0,   1,   1],
-                [0,   0,   0,   1],
-                [1,   0,   1,   1],
-            ];
-            scope.calculate_next_generation(0,1);
-            expect(scope.game_board[0][1]).toBe(0);
-        });
-
-        it('Should Any live cell with two or three live neighbours lives on to the next generation.', function () {
-            scope.original_game_board = [
-                [0, 1, 0],
-                [1, 1, 0],
-                [0, 1, 0]
-            ];            
-            scope.calculate_next_generation(1,0);
-            expect(scope.game_board[1][0]).toBe(1);
-
-            scope.original_game_board = [
-                [0, 1, 0],
-                [1, 1, 0],
-                [0, 0, 0]
-            ];            
-            scope.calculate_next_generation(1,0);
-            expect(scope.game_board[1][0]).toBe(1);           
-
-            scope.original_game_board = [
-                [0, 1, 0],
-                [0, 1, 1],
-                [1, 1, 1]
-            ];
-            scope.calculate_next_generation(0, 1);
-            expect(scope.game_board[0][1]).toBe(1);
-        });
-
-        it('Should Any live cell with more than three live neighbours dies, as if by over-population.', function () {
-            scope.original_game_board = [
-                [0, 1, 0],
-                [1, 1, 0],
-                [1, 1, 0]
-            ];            
-            scope.calculate_next_generation(1,1);
-            expect(scope.game_board[1][1]).toBe(0);            
-        });     
-        
-        iit('should Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.', function () {
-            scope.original_game_board = [
-                [0, 0, 1],
-                [0, 1, 1],
-                [1, 1, 1]
-            ];
-            scope.calculate_next_generation(0, 1);
-            expect(scope.game_board[0][1]).toBe(1);
-
-            scope.original_game_board = [
-                [0, 0, 1],
-                [0, 1, 1],
-                [1, 1, 1]
-            ];
-            scope.calculate_next_generation(1, 0);
-            expect(scope.game_board[1][0]).toBe(1);         
-
-            scope.original_game_board = [
-                [0, 0, 0, 0, 0],
-                [0, 0, 1, 0, 0],
-                [0, 0, 1, 0, 0],
-                [0, 0, 1, 0, 0],
-                [0, 0, 0, 0, 0]
-            ];
-
-            scope.calculate_next_generation(2, 1);
-            expect(scope.game_board[2][1]).toBe(1);      
-
-        });         
-
-    });
+  });
 });
